42bf253cbd9335c8f31ca572c3f2391cd0d03023
kasi-iyo <snakeman143@gmail.com>
2020-08-07 17:33:30 +0900

    投稿を保存するAddPostFormを作成

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { nanoid } from '@reduxjs/toolkit'
+
+import { postAdded } from './postsSlice'
+
+export const AddPostForm = () => {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    const dispatch = useDispatch()
+
+    const onTitleChanged = e => setTitle(e.target.value)
+    const onContentChanged = e => setContent(e.target.value)
+
+    const onSavePostClicked = () => {
+        if (title && content) {
+            dispatch(
+                postAdded({
+                    id: nanoid(),
+                    title,
+                    content
+                })
+            )
+
+            setTitle('')
+            setContent('')
+        }
+    }
+
+    return (
+        <section>
+            <h2>投稿を追加する</h2>
+            <form>
+                <label htmlFor="postTitle">タイトル: </label>
+                <input
+                    type="text"
+                    id="postTitle"
+                    name="postTitle"
+                    value={title}
+                    onChange={onTitleChanged}
+                />
+                <label htmlFor="postContent">内容: </label>
+                <textarea
+                    id="postContent"
+                    name="postContent"
+                    value={content}
+                    onChange={onContentChanged}
+                />
+                <button type="button" onClick={onSavePostClicked}>
+                    保存
+                </button>
+            </form>
+        </section>
+    )
+}
