33732e594284add6baca6785f030dc4deccd1d86
kasi-iyo <snakeman143@gmail.com>
2020-08-12 16:14:41 +0900

    PostAuthorコンポーネント／TimeAgoコンポーネント／ReactionButtonsコンポーネントをimport

--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -2,16 +2,25 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
+
 export const PostsList = () => {
     const posts = useSelector(state => state.posts)
 
-    const renderedPosts = posts.map(post => (
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+    const renderedPosts = orderedPosts.map(post => (
         <article className="post-excerpt">
             <h4>{post.title}</h4>
             <p>{post.content.substring(0, 100)}</p>
             <Link to={`/posts/${post.id}`} className="button muted-button">
                 詳細を見る
             </Link>
+            <ReactionButtons post={post} />
+            <PostAuthor userId={post.user} />
+            <TimeAgo timestamp={post.date} />
         </article>
     ))
 
