f233c342f86f19972c7eb541939216278a173267
kasi-iyo <snakeman143@gmail.com>
2020-08-14 22:06:10 +0900

    postsListコンポーネントで、postsオブジェクトのstatusによってUIを再レンダリングする。

--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -7,22 +7,9 @@ import { TimeAgo } from './TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
 import { selectAllPosts, fetchPosts } from './postsSlice'
 
-export const PostsList = () => {
-    const dispatch = useDispatch()
-    const posts = useSelector(selectAllPosts)
-
-    const postsStatus = useSelector(state => state.posts.status)
-
-    useEffect(() => {
-        if (postsStatus === 'idle') {
-            dispatch(fetchPosts())
-        }
-    }, [postsStatus, dispatch])
-
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-
-    const renderedPosts = orderedPosts.map(post => (
-        <article className="post-excerpt">
+const PostExcerpt = ({ post }) => {
+    return (
+        <article className="post-excerpt" key={post.id}>
             <h4>{post.title}</h4>
             <p>{post.content.substring(0, 100)}</p>
             <Link to={`/posts/${post.id}`} className="button muted-button">
@@ -32,12 +19,39 @@ export const PostsList = () => {
             <PostAuthor userId={post.user} />
             <TimeAgo timestamp={post.date} />
         </article>
-    ))
+    )
+}
+
+export const PostsList = () => {
+    const dispatch = useDispatch()
+    const posts = useSelector(selectAllPosts)
+
+    const postsStatus = useSelector(state => state.posts.status)
+    const error = useSelector(state => state.posts.error)
+
+    useEffect(() => {
+        if (postsStatus === 'idle') {
+            dispatch(fetchPosts())
+        }
+    }, [postsStatus, dispatch])
+
+    let content
+    if (postsStatus === 'loading') {
+        content = <div className="loader">Loading...</div>
+    } else if (postsStatus === 'succeeded') {
+        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+        content = orderedPosts.map(post => 
+            <PostExcerpt key={post.id} post={post} />
+        )
+    } else if (postsStatus === 'failed') {
+        content = <div>{error}</div>
+    }
 
     return (
         <section>
             <h2>Posts</h2>
-            {renderedPosts}
+            {content}
         </section>
     )
 }
